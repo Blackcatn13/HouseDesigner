8d0163eb627403b5e960401fa917c7bcd2f0adc0
Aitor <acriadovazquez@bitbucket.org>
2013-04-28 23:34:47 +0200

    Improved Vision of camera
    Former-commit-id: c6ce97a6f5767b0ef18624cd72f01712135ec752

--- CameraPerspective.cpp
+++ CameraPerspective.cpp
@@ -10,7 +10,7 @@ CameraPerspective::CameraPerspective()
     glLoadIdentity();
 
     // Set default values for camera and projection
-    R      = 2.5;
+    R      = 25;
     angleh = 15;
     anglev = 35;
     Cnear   = .1;
@@ -37,41 +37,31 @@ void CameraPerspective::setProjection(int w, int h)
 void CameraPerspective::update()
 {
     glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+    glLoadIdentity();
 
-	float anglex,angley;
-	GLfloat cam[3],up[3];
+    float anglex,angley;
+    GLfloat cam[3],up[3];
 
-	// Rad -> Deg
-	anglex=angleh*PI/180;
-	angley=anglev*PI/180;
-		
-	// Cam position and up-vector
-	cam[0]=R*cos(anglex)*cos(angley);
-	cam[1]=R*sin(anglex)*cos(angley);
-	cam[2]=R*sin(angley);
-	//up[0]=-cos(anglex)*sin(angley);
-	//up[1]=-sin(anglex)*sin(angley);
-	//up[2]=cos(angley);
-	up[0] = 0;
-	up[1] = 1;
-	up[2] = 0;
+    // Rad -> Deg
+    anglex=angleh*PI/180;
+    angley=anglev*PI/180;
 
-	// Set camera in OpenGL
-	gluLookAt(cam[0],cam[1],cam[2],
-				0.0,0.0,0.0,
-				up[0],up[1],up[2]);
+    // Cam position and up-vector
+    cam[0]=R*cos(anglex)*cos(angley);
+    cam[1]=R*sin(anglex)*cos(angley);
+    cam[2]=R*sin(angley);
+
+
+    // Set camera in OpenGL
+    gluLookAt(cam[0],cam[1],cam[2],
+                0.0,0.0,0.0,
+                0,1,0);
 }
 
 void CameraPerspective::move(float ah, float av)
 {
-    angleh += ah/2;
-    anglev -= av/2;
-
-    if(anglev>=360)	anglev=anglev-360;
-    if(anglev<0)	anglev=anglev+360;
-    if(angleh>=360)	angleh=angleh-360;
-    if(angleh<0)	angleh=angleh+360;
+    angleh += ah;
+    anglev += av;
 }
 
 void CameraPerspective::AddDistance(float d)
