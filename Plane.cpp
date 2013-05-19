79176db33fc6db2f72a71a9f4653b2c8d7d56c0f
Adrian Antunez <adri25_sbd@hotmail.com>
2013-05-19 14:41:52 +0200

    Fixed some changes.
    Former-commit-id: 4cfb13e62d86d83d800ac997eb57106b2a91e047

--- Plane.cpp
+++ Plane.cpp
@@ -4,7 +4,7 @@ CPlane::CPlane()
 {
 }
 
-void CPlane::set3Points(CPoint3D &p1, CPoint3D &p2, CPoint3D &p3)
+void CPlane::set3Points(CPoint3D p1, CPoint3D p2, CPoint3D p3)
 {
     CPoint3D aux, aux1;
     //Calculate vectors.
@@ -17,16 +17,16 @@ void CPlane::set3Points(CPoint3D &p1, CPoint3D &p2, CPoint3D &p3)
     mD = acos(mNormal.Dot(p2)/(mNormal.Norma()*p2.Norma()))*-1;
 }
 
-void CPlane::setNormalAndPoint(CPoint3D &normal, CPoint3D &point)
+void CPlane::setNormalAndPoint(CPoint3D normal, CPoint3D point)
 {
     mNormal = normal;
     mPoint = point;
     //Calculate angle between normal and p2 vectors
-    mD = acos(mNormal.Dot(p2)/(mNormal.Norma()*p2.Norma()))*-1;
+    mD = acos(mNormal.Dot(point)/(mNormal.Norma()*point.Norma()))*-1;
 }
 
-float CPlane::distance(CPoint3D &p)
+float CPlane::distance(CPoint3D point)
 {
     //Return distance (d is negative + distance p positive).
-    return d + acos(mNormal.Dot(p2)/(mNormal.Norma()*p2.Norma()));
+    return mD + acos(mNormal.Dot(point)/(mNormal.Norma()*point.Norma()));
 }
--- Plane.h
+++ Plane.h
@@ -9,12 +9,12 @@ class CPlane
 public:
     CPlane(void);
     //Set plane with 3 points.
-    void set3Points (CPoint3D &v1, CPoint3D &v2, CPoint3D &v3);
+    void set3Points (CPoint3D v1, CPoint3D v2, CPoint3D v3);
     //Set plane with Normal and a point.
-    void setNormalAndPoint(CPoint3D &normal, CPoint3D &point);
+    void setNormalAndPoint(CPoint3D normal, CPoint3D point);
     //Set plane using Ax + By + Cz + D equation.
     void setCoefficients(float a, float b, float c, float d);
-    float distance(CPoint3D &p);
+    float distance(CPoint3D point);
 
 private:
     CPoint3D mNormal, mPoint;
