ecfffcf9669f9e7284d85bc00dc939c51cdf953d
Adrian Antunez <adri25_sbd@hotmail.com>
2013-05-19 14:39:22 +0200

    Created new class to manage planes for frustum culling.
    Former-commit-id: 482a7cb9bae45e81a99e73ab8281413dcaf6a57d

---
+++ Plane.cpp
@@ -0,0 +1,32 @@
+#include "Plane.h"
+
+CPlane::CPlane()
+{
+}
+
+void CPlane::set3Points(CPoint3D &p1, CPoint3D &p2, CPoint3D &p3)
+{
+    CPoint3D aux, aux1;
+    //Calculate vectors.
+    aux = p1 - p2;
+    aux1 = p3 - p2;
+
+    mNormal = aux1 * aux;
+    mNormal.Normalize();
+    //Calculate angle between normal and p2 vectors.
+    mD = acos(mNormal.Dot(p2)/(mNormal.Norma()*p2.Norma()))*-1;
+}
+
+void CPlane::setNormalAndPoint(CPoint3D &normal, CPoint3D &point)
+{
+    mNormal = normal;
+    mPoint = point;
+    //Calculate angle between normal and p2 vectors
+    mD = acos(mNormal.Dot(p2)/(mNormal.Norma()*p2.Norma()))*-1;
+}
+
+float CPlane::distance(CPoint3D &p)
+{
+    //Return distance (d is negative + distance p positive).
+    return d + acos(mNormal.Dot(p2)/(mNormal.Norma()*p2.Norma()));
+}
---
+++ Plane.h
@@ -0,0 +1,25 @@
+#ifndef PLANE_H
+#define PLANE_H
+
+#include "Point3D.h"
+#include "Util.h"
+
+class CPlane
+{
+public:
+    CPlane(void);
+    //Set plane with 3 points.
+    void set3Points (CPoint3D &v1, CPoint3D &v2, CPoint3D &v3);
+    //Set plane with Normal and a point.
+    void setNormalAndPoint(CPoint3D &normal, CPoint3D &point);
+    //Set plane using Ax + By + Cz + D equation.
+    void setCoefficients(float a, float b, float c, float d);
+    float distance(CPoint3D &p);
+
+private:
+    CPoint3D mNormal, mPoint;
+    float mD;
+
+};
+
+#endif // PLANE_H
